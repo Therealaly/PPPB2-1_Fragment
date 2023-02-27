f3b847406263ceac602214c6906eb7ef160e7e8c
Therealaly <aadley85@gmail.com>
2023-02-27 18:22:21 +0700

    dynamic fragment

--- app/src/main/java/com/example/fragment_app/ButtonFragment.java
+++ app/src/main/java/com/example/fragment_app/ButtonFragment.java
@@ -8,8 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ButtonFragment extends Fragment {
+
+    private boolean counter;
 
     public ButtonFragment() {
         // Required empty public constructor
@@ -21,11 +26,21 @@ public class ButtonFragment extends Fragment {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_button, container, false);
         final Button button = rootView.findViewById(R.id.button);
+        final TextView desc = rootView.findViewById(R.id.main_desc);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                button.setText(R.string.close_button);
+                if (counter == false){
+                    button.setText(R.string.close_button);
+                    desc.setText(R.string.details_desc);
+                    counter = true;
+                }else if(counter != false ){
+                    button.setText(R.string.default_button);
+                    desc.setText(R.string.main_desc);
+                    counter = false;
+                }
+
             }
         });
 
